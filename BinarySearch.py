275444b65e52bc78d3c29802b907ba59d43c3212
mary gangla <mgangla@gmail.com>
2024-02-07 19:05:38 +0300

    initial commit

--- BinarySearch.py
+++ BinarySearch.py
@@ -8,6 +8,10 @@ class BSAlgo:
         self.right = None
         self.val = val
 
+    #A new key is always inserted at the leaf by maintaining the property of the binary search tree. 
+    #We start searching for a key from the root until we hit a leaf node. 
+    #Once a leaf node is found, the new node is added as a child of the leaf node
+
     def insert(self, val):
         if not self.val:
             self.val = val
@@ -40,9 +44,48 @@ class BSAlgo:
         while current.right is not None:
             current = current.right
         return current.val
-    
+   
+
     # function to order incase of a deletion
-    #function to order 
+    # 3scenarios
+
+    #1.Deleting a single child node is also simple in BST. 
+    #Copy the child to the node and delete the node. 
+
+    #2. Delete a Leaf Node in BST
+    #3.Delete a Node with Both Children in BST
+    # Here we have to delete the node is such a way,
+    # that the resulting tree follows the properties of a BST.  
+
+    #we need to find the inorder successor of the node. 
+    #Copy contents of the inorder successor to the node, and delete the inorder successor.
+
+    def delete(self, val):
+        if self == None:
+            return self
+        if val < self.val:
+            self.left = self.left.delete(val)
+            return self
+        if val > self.val:
+            self.right = self.right.delete(val)
+            return self
+        if self.right == None:
+            return self.left
+        if self.left == None:
+            return self.right
+        min_larger_node = self.right
+        while min_larger_node.left:
+            min_larger_node = min_larger_node.left
+        self.val = min_larger_node.val
+        self.right = self.right.delete(min_larger_node.val)
+        return self
+    
+
+    #function to order## Traversals
+    #Inorder, Preorder, Post Order
+    #Helps print out the tree in a readable format. 
+    #The inorder method print’s the values in the tree in the order of their keys.
+
 
 #call the functions
     
